7fa79d033d726b6975a347076c5fc85fcbd4e6f0
Milad704 <miladgobbo@gmail.com>
2025-11-27 14:14:46 -0800

    added godot to the github

---
+++ appledropper/betterbasket.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+var score = 0
+var score_label 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	score_label = get_node("/root/Node2D/ScoreLabel")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_key_pressed(KEY_LEFT):
+		position.x -= 8
+	elif Input.is_key_pressed(KEY_RIGHT):
+		position.x += 8
+	
+
+	pass
+
+
+func _on_body_entered(body):
+	print("i am being touched");
+	score += 1
+	score_label.text = "Score:" + str(score)
+	pass # Replace with function body.
