0e0e9a18f0491689a8a9c44e976ec445ed06bc24
Milad704 <miladgobbo@gmail.com>
2025-12-08 13:31:21 -0800

    made a snowflake the reduces speed of player/ player jumps as well

--- appledropper/betterbasket.gd
+++ appledropper/betterbasket.gd
@@ -1,26 +1,49 @@
 extends Area2D
 
+var score_label
+var obstacle
 
-#var score = 0
-var score_label 
-# Called when the node enters the scene tree for the first time.
+var vel_y = 0
+var gravityy = 200
+var jump_force = -200
+var floor_y = 629
+var can_jump = true
+
+var normal_speed = 300
+var speed = 300
+
 func _ready():
 	score_label = get_node("/root/Node2D/ScoreLabel")
-	pass # Replace with function body.
+	obstacle = get_node("/root/Node2D/Obstacle")  # your obstacle node
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_key_pressed(KEY_LEFT):
-		position.x -= 8
+		position.x -= speed * delta
 	elif Input.is_key_pressed(KEY_RIGHT):
-		position.x += 8
-	pass
+		position.x += speed * delta
+
+	vel_y += gravityy * delta
+	position.y += vel_y * delta
+
+	if position.y >= floor_y:
+		position.y = floor_y
+		vel_y = 0
+		can_jump = true
+
+	if can_jump and Input.is_key_pressed(KEY_UP):
+		vel_y = jump_force
+		can_jump = false
 
 
 func _on_body_entered(body):
-	print("i am being touched");
-	Global.score += 1
-	score_label.text = "Score:" + str(Global.score)
+	if body == obstacle:
+		speed = 30   # <-- slow to 30
+	else:
+		Global.score += 1
+		if score_label:
+			score_label.text = "Score:" + str(Global.score)
 
-	pass # Replace with function body.
+
+func _on_body_exited(body):
+	if body == obstacle:
+		speed = normal_speed   # restore
---
+++ appledropper/obstacle.gd
@@ -0,0 +1,6 @@
+extends StaticBody2D
+
+func _on_timer_timeout():
+	position.x = randi_range(0, 1100)
+	position.y = 626
+	pass # Replace with function body.
