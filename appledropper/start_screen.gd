305189d0e787bc2fe3d0157b2c1e28d04d6f8d09
HenryHenry0908 <lir95951@gmail.com>
2026-01-06 14:14:59 -0800

    add skin select page

--- appledropper/start_screen.gd
+++ appledropper/start_screen.gd
@@ -3,3 +3,8 @@ extends Node2D
 
 func _on_start_button_down():
 	get_tree().change_scene_to_file("res://betterapple.tscn")
+
+
+
+func _on_skin_page_button_down() -> void:
+	get_tree().change_scene_to_file("res://store_page.tscn")
